b25aa5324ceec2f08e5e69c942cfd12193a8d73d
printertechiesbuddy <printertechiesbuddy@gmail.com>
2024-10-17 01:49:43 +0530

    Add files via upload

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-281f53b.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-b84af7dc', {"layout":"number","heading":"Short Description","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":93,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-2fa9954c', {"layout":"number","heading":"Short Description","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":175,"totalNumber":175,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-14ef0b0a', {"layout":"number","heading":"Short Description","numberPrefix":"","numberSuffix":"K","startNumber":0,"endNumber":30,"totalNumber":175,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-dcd05062', {"layout":"number","heading":"Short Description","numberPrefix":"","numberSuffix":"Mil","startNumber":0,"endNumber":1.5,"totalNumber":175,"decimalPlaces":1,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-285f53b.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"0e1719e7","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"Support@wingsprinter.online","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-0e1719e7', 285 );
+});
+ });
